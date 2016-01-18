26d2143f75b44f4e0c8ee201e8f4a4a04b704e8d
Andrew Vy <andrew@andrewvy.com>
2016-01-18 09:00:34 -0800

    Folder restructure

---
+++ todolist/Classes/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  todolist
+//
+//  Created by Andrew Vy on 1/17/16.
+//  Copyright © 2016 Andrew Vy. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    static let host = "localhost:8000"
+    static let version = "v1"
+    static let scheme = "http"
+}
